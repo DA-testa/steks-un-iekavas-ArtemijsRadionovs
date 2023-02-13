7d109dd5cdb310f298dfd037a5c3dae14a2e7a9d
ArtemijsRadionovs <124582137+ArtemijsRadionovs@users.noreply.github.com>
2023-02-13 10:13:03 +0200

    The Last 3

--- main.py
+++ main.py
@@ -25,9 +25,27 @@ def find_mismatch(text):
     return "Success"
 
 def main():
-    text = input().strip()
-    text = text[5:]
-    result = find_mismatch(text)
+#     text = input().strip()
+#     text = text[5:]
+#     result = find_mismatch(text)
+    
+    def main():
+    result = 0
+    check_for_file = input()
+    if check_for_file.lower() == "f":
+        file = input()
+        with open(file, "r") as f:
+            file_code = f.read()
+            result = find_mismatch(file_code)
+    elif check_for_file == "i":
+        text = input()
+        result = find_mismatch(text)
+    
+    if isinstance(result, int):
+        print(result)
+    else:
+        print(result)
+
     if isinstance(result, int):
         print(result)
     else:
